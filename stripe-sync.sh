4ee1df4f9d5ef57b969b8d8934e2a559b539b7db
Kai Hendry <hendry@iki.fi>
2020-09-23 17:59:44 +0800

    Implementing https://stripe.com/docs/billing/prices-guide in shell

--- stripe-sync.sh
+++ stripe-sync.sh
@@ -1,6 +1,11 @@
-#!/bin/bash
+#!/bin/bash -e
 # https://stripe.com/docs/billing/prices-guide
-stripesecret=sk_test_fAZaJSuP4s2LQlu45SOFGFQO:
+source .env
+if ! test "$STRIPE_SECRET"
+then
+	echo Please source \$STRIPE_SECRET
+	exit
+fi
 
 for p in public/*/index.json
 do
@@ -9,9 +14,19 @@ jq -r '.[]|.name,.sku,.price' $p |
 	read -r sku
 	read -r price
 	do
+
 	printf "Name: %s SKU: %s Price: %s\n" "$name" "$sku" "$price"
-	stripeproduct=data/products/$sku.json
-	test -f $stripeproduct || curl https://api.stripe.com/v1/products -u $stripesecret -d name="$name" > $stripeproduct
-	jq < $stripeproduct
+
+	stripeproduct=data/product/$sku.json
+	stripeprice=data/price/$sku.json
+
+	test -s $stripeproduct || curl https://api.stripe.com/v1/products -u ${STRIPE_SECRET}: -d name="$name" > $stripeproduct
+	prodid=$(jq -r '.id' < $stripeproduct)
+	echo Product ID: $prodid
+
+	test -s $stripeprice || curl https://api.stripe.com/v1/prices -u ${STRIPE_SECRET}: -d product="$prodid" -d unit_amount="$price" -d currency=${CURRENCY:-"usd"} > $stripeprice
+	priceid=$(jq -r '.id' < $stripeprice)
+	echo Price ID: $priceid
+
 	done
 done
