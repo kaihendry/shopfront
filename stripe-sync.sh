c2a6f8c5bdbf3054abeda2949fbbd0fffd4e5a36
Kai Hendry <hendry@iki.fi>
2020-09-23 18:06:55 +0800

    Shellcheck to rain on the parade

--- stripe-sync.sh
+++ stripe-sync.sh
@@ -1,6 +1,14 @@
 #!/bin/bash -e
 # https://stripe.com/docs/billing/prices-guide
-source .env
+
+if test -f .env
+then
+	source .env
+else
+	echo Missing environment file: .env
+	exit
+fi
+
 if ! test "$STRIPE_SECRET"
 then
 	echo Please source \$STRIPE_SECRET
@@ -9,24 +17,26 @@ fi
 
 for p in public/*/index.json
 do
-jq -r '.[]|.name,.sku,.price' $p |
+	jq -r '.[]|.name,.sku,.price' "$p" |
 	while read -r name
-	read -r sku
-	read -r price
-	do
+		read -r sku
+		read -r price
+		do
 
-	printf "Name: %s SKU: %s Price: %s\n" "$name" "$sku" "$price"
+		printf "Name: %s SKU: %s Price: %s\n" "$name" "$sku" "$price"
 
-	stripeproduct=data/product/$sku.json
-	stripeprice=data/price/$sku.json
+		stripeproduct=data/product/$sku.json
+		stripeprice=data/price/$sku.json
 
-	test -s $stripeproduct || curl https://api.stripe.com/v1/products -u ${STRIPE_SECRET}: -d name="$name" > $stripeproduct
-	prodid=$(jq -r '.id' < $stripeproduct)
-	echo Product ID: $prodid
+		test -s "$stripeproduct" ||
+		curl https://api.stripe.com/v1/products -u "${STRIPE_SECRET}:" -d name="$name" > "$stripeproduct"
+		prodid=$(jq -r '.id' < "$stripeproduct")
+		echo Product ID: "$prodid"
 
-	test -s $stripeprice || curl https://api.stripe.com/v1/prices -u ${STRIPE_SECRET}: -d product="$prodid" -d unit_amount="$price" -d currency=${CURRENCY:-"usd"} > $stripeprice
-	priceid=$(jq -r '.id' < $stripeprice)
-	echo Price ID: $priceid
+		test -s "$stripeprice" ||
+		curl https://api.stripe.com/v1/prices -u "${STRIPE_SECRET}:" -d product="$prodid" -d unit_amount="$price" -d currency="${CURRENCY:-"usd"}" > "$stripeprice"
+		priceid=$(jq -r '.id' < "$stripeprice")
+		echo Price ID: "$priceid"
 
 	done
 done
